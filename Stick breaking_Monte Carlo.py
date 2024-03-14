7d1f10579447a62628d0c09d0a56bde63077c411
Vacuum Bubble <andrearusso.quant@gmail.com>
2024-03-14 18:41:13 +0000

    Implement feature/fix for issue #2: Renamed variables for clarity and fix minor comment errors

--- Stick breaking_Monte Carlo.py
+++ Stick breaking_Monte Carlo.py
@@ -7,29 +7,30 @@ what is the probability that you could form a triangle with the three pieces?
 # Import libraries
 import numpy.random as rnd
 
-# Set number of Monte Carlo simulations
-samples = 10000
+# Set up number of Monte Carlo simulations
+n_samples = 10000
 
 # Generate breaking points of the stick
-X = rnd.uniform(0, 1, samples)
-Y = rnd.uniform(0, 1, samples)
+X = rnd.uniform(0, 1, n_samples)
+Y = rnd.uniform(0, 1, n_samples)
 
-# This variable counts the numbe rof triangles formed
+# This variable counts the number of triangles formed
 triangle = 0
 
 # Monte carlo simulation
-for i in range(samples):
+for i in range(n_samples):
     
     # It is important to set the correct min/max variable as L1 and L3
-    L1 = min(X[i], Y[i])
-    L2 = abs(Y[i] - X[i])
-    L3 = 1 - max(X[i], Y[i])
+    Lenght_1 = min(X[i], Y[i])
+    Lenght_2 = abs(Y[i] - X[i])
+    Lenght_3 = 1 - max(X[i], Y[i])
     
     # If it is a triangle, count it
-    if (L1 + L2 > L3) and  (L1 + L3 > L2) and (L2 + L3 > L1):
+    if (Lenght_1 + Lenght_2 > Lenght_3) and  (Lenght_1 + Lenght_3 > Lenght_2) and (Lenght_2 + Lenght_3 > Lenght_1):
         triangle += 1
 
 # Compute probability of triangle forming
-prob = round(triangle/samples,3)
+prob = round(triangle / n_samples,3)
     
+# Print resulting probability
 print(f"The probability of getting a triangle is: {prob*100}%")
